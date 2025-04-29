8496fb25cf64865949b0a757109481b8d7effc7c
Murat Yıldırım <murat505yldrmm@gmail.com>
2025-04-29 12:45:09 +0300

    `updated Drivers.js to fetch and display drivers data using TableComp component`

---
+++ frontend/src/components/TableComp.js
@@ -0,0 +1,85 @@
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import { Typography } from '@mui/material';
+import { useState } from 'react';
+
+
+
+
+export default function TableComp({ columns, rows }) {
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
+
+    return (
+        <Paper sx={{ width: '100%', overflow: 'hidden', marginTop: "10px" }}>
+            <Typography variant='h4' sx={{ margin: "10px", }}>Drivers</Typography>
+            <TableContainer sx={{ maxHeight: 440 }}>
+                <Table stickyHeader aria-label="sticky table">
+                    <TableHead>
+                        <TableRow>
+                            {columns.map((column) => (
+                                <TableCell
+                                    key={column.id}
+                                    align={column.align}
+                                    style={{ minWidth: column.minWidth }}
+                                >
+                                    {column.label}
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.length === 0 ? (
+                            <TableRow>
+                                <TableCell colSpan={columns.length} align="center">
+                                    No data found.
+                                </TableCell>
+                            </TableRow>
+                        ) : (
+                            rows
+                                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                .map((row) => (
+                                    <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                                        {columns.map((column) => {
+                                            const value = row[column.id];
+                                            return (
+                                                <TableCell key={column.id} align={column.align}>
+                                                    {column.format && typeof value === "number"
+                                                        ? column.format(value)
+                                                        : value}
+                                                </TableCell>
+                                            );
+                                        })}
+                                    </TableRow>
+                                ))
+                        )}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <TablePagination
+                rowsPerPageOptions={[10, 25, 100]}
+                component="div"
+                count={rows.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+        </Paper>
+    );
+}
--- frontend/src/pages/Drivers.js
+++ frontend/src/pages/Drivers.js
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import TableComp from '../components/TableComp'
+import { fetchDrivers } from '../services/api';
+
+const columns = [
+    { id: 'name', label: 'Name', minWidth: 170 },
+    { id: 'licenseNumber', label: 'License Number', minWidth: 100 },
+    {
+        id: 'phone',
+        label: 'Phone',
+        minWidth: 170,
+        align: 'right',
+    },
+    {
+        id: 'assignedVehicle',
+        label: 'Vehicle',
+        minWidth: 170,
+        align: 'right',
+    },
+];
+
 
 export const Drivers = () => {
+    const [drivers, setDrivers] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async() => {
+            const driversData = await fetchDrivers();
+            setDrivers(driversData);
+        }
+
+        fetchData();
+    }, [])
+
+    const rows = drivers.map((driver) => ({
+        name: driver.name,
+        licenseNumber: driver.licenseNumber,
+        phone: driver.phone,
+        assignedVehicle: driver.assignedVehicle ? driver.assignedVehicle.plateNumber : "Empty",
+    } ))
+
     return (
-        <div>Drivers</div>
+        <TableComp columns={columns} rows={rows}></TableComp>
     )
 }
